915c77dadd741e6c1d6d555772f11ec9b50d3719
QuantumJaguar <thomaslong161@gmail.com>
2024-11-01 23:11:48 +0530

    app js changed

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React App
+            Learn React ..
           </a>
         </header>
         <div className="content">
