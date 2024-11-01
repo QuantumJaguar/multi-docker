6cd0df848e6020dc3f1a1955e4e33791d1182ea8
QuantumJaguar <thomaslong161@gmail.com>
2024-11-01 14:47:53 +0530

    travis settings changed

--- client/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    "@babel/plugin-proposal-logical-assignment-operators",
-    "@babel/plugin-proposal-private-property-in-object",
-  ],
-};
--- client/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest",
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!@testing-library|react-scripts).+\\.js$",
-  ],
-};
